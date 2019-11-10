692655d6bba95bca9ed0d6bb4b286c6d0b85b23f
Prashant Singh <prashant_singh@live.in>
2019-11-10 02:53:19 -0600

    hackerrank: func skeleton for 'Counting Valleys'

--- hackerrank/counting_valleys.py
+++ hackerrank/counting_valleys.py
@@ -40,3 +40,26 @@
 #     \/\/
 # He enters and leaves one valley.
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
