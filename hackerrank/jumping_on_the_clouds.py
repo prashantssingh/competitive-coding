8fa8ffee4a5ab2bd8cabac10b9a45b5e1e3ecc32
Prashant Singh <prashant_singh@live.in>
2019-11-10 15:25:35 -0600

    hackerrank: func skeleton for `Jumping on the Clouds`

--- hackerrank/jumping_on_the_clouds.py
+++ hackerrank/jumping_on_the_clouds.py
@@ -38,3 +38,26 @@
 # EXPLANATION:
 # The only thundercloud to avoid is `c[4]`. Emma can win the game in `3` jumps: `0->2->3->5`
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
