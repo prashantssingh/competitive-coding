4179cb41577d550cdfe45d77d7fad74c0fccecb7
Prashant Singh <prashant_singh@live.in>
2019-11-10 19:44:30 -0600

    hackerrank: func skeleton for `Repeated String`

--- hackerrank/repeated_string.py
+++ hackerrank/repeated_string.py
@@ -30,3 +30,27 @@
 
 # EXPLANATION
 # Because all of the first `n = 1000000000000` letters of the infinite string are a, we print n on a new line.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
