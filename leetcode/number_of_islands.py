24fdb8a985693e07d926bc2e12b196833a9a5107
Prashant Singh <prashant_singh@live.in>
2019-11-13 20:48:54 -0600

    leetcode: prob_desc for `Number of Islands`

---
+++ leetcode/number_of_islands.py
@@ -0,0 +1,19 @@
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is 
+# surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You 
+# may assume all four edges of the grid are all surrounded by water.
+
+# EXAMPLE 1:
+# INPUT                                      OUTPUT:
+# 11110                                      1
+# 11010
+# 11000
+# 00000
+
+
+# EXAMPLE 2:
+# INPUT                                      OUTPUT:
+# 11000                                       3
+# 11000
+# 00100
+# 00011
+
