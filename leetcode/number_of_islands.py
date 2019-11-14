319548c5eb9ec8c1d6c35b4dde444ffb3312f20b
Prashant Singh <prashant_singh@live.in>
2019-11-13 20:50:39 -0600

    leetcode: solution for `Number of Islands`

--- leetcode/number_of_islands.py
+++ leetcode/number_of_islands.py
@@ -18,3 +18,21 @@
 # 00100
 # 00011
 
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islandMarker = -1
+        visitedGrid = grid
+        for rowIndex in range(len(grid)):
+            for colIndex in range(len(grid[rowIndex])):
+                if rowIndex == 0 and colIndex == 0 and grid[rowIndex][colIndex] == 1:
+                    visitedGrid[rowIndex][colIndex] = islandMarker
+                    islandMarker += 1
+                elif rowIndex > 0 and grid[rowIndex-1][colIndex]<0:
+                    visitedGrid[rowIndex][colIndex] = visitedGrid[rowIndex-1][colIndex]
+                elif colIndex > 0 and grid[rowIndex][colIndex-1]<0:
+                    visitedGrid[rowIndex][colIndex] = visitedGrid[rowIndex][colIndex-1]
+                else:
+                    visitedGrid[rowIndex][colIndex] = islandMarker
+                    islandMarker += 1
+                    
+        return abs(islandMarker+1)
