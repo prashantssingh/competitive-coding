5ada7f787e11df72e86280e64a0063c9ff6c4c82
Prashant Singh <prashant_singh@live.in>
2019-11-16 19:50:14 -0600

    leetcode: prob-desc for `Palindrome Number`

---
+++ leetcode/plaindrome_number.py
@@ -0,0 +1,15 @@
+# PROBLEM DESCRIPTION
+# Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+# SAMPLE INPUT                                      SAMPLE OUTPUT
+# 121                                               true 
+
+# SAMPLE INPUT                                      SAMPLE OUTPUT
+# 121                                               false
+# EXPLANATION
+# From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+# SAMPLE INPUT                                      SAMPLE OUTPUT
+# 10                                                false
+# EXPLANATION
+# Reads 01 from right to left. Therefore it is not a palindrome.
