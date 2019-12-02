79f59bf4daf2e1ecc7316bf0a591ccb277d8812b
Prashant Singh <prashant_singh@live.in>
2019-12-01 21:57:26 -0600

    leetcode: func skeleton for `3Sum`

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -12,3 +12,7 @@
 # Given array nums = ,
 # INPUT                                               OUTPUT:
 # [-1, 0, 1, 2, -1, -4]                               [ [-1, 0, 1], [-1, -1, 2] ]
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        pass
