20a392f7a75d568cd451e13197344883f979ceda
Prashant Singh <prashant_singh@live.in>
2019-12-01 21:28:03 -0600

    leetcode: prob-desc for `3Sum`

---
+++ leetcode/3sum.py
@@ -0,0 +1,14 @@
+# OROBLEM STATEMENT
+# Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+# Find all unique triplets in the array which gives the sum of zero.
+
+# NOTE
+# The solution set must not contain duplicate triplets.
+
+# Example:
+
+
+# EXAMPLES:
+# Given array nums = ,
+# INPUT                                               OUTPUT:
+# [-1, 0, 1, 2, -1, -4]                               [ [-1, 0, 1], [-1, -1, 2] ]
