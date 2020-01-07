fe953b3972ba16e849da1f9c21485aec43481b33
Prashant Singh <prashant_singh@live.in>
2020-01-06 23:35:35 -0600

    leetcode: soln for `Pascal's Traingle`

--- leetcode/pascal_triangle.py
+++ leetcode/pascal_triangle.py
@@ -16,3 +16,22 @@
 #                                                 [1,4,6,4,1]
 #                                                ]
 
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0: return []
+        if numRows == 1: return [[1]]
+        if numRows == 2: return [[1], [1,1]]
+        
+        result = [[1], [1,1]]
+        for i in range (2, numRows):
+            curr = [0]*(i+1)
+            curr[0] = curr[i] = 1
+            j, k = 1, i-1
+            while j <= k:
+                curr[j] = curr[k] = result[i-1][j-1] + result[i-1][j]
+                j += 1
+                k -= 1
+            result.append(curr)
+            
+        return result
+        
