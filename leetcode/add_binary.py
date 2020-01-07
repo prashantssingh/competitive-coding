8138cdea508aa0aa2f9e9bef9c7f695d5d689bd5
Prashant Singh <prashant_singh@live.in>
2020-01-06 23:20:04 -0600

    leetcode: prob-desc for `Add Binary`

---
+++ leetcode/add_binary.py
@@ -0,0 +1,13 @@
+# PROBLEM DESCRIPTION
+# Given two binary strings, return their sum (also a binary string).
+# The input strings are both non-empty and contains only characters 1 or 0.
+
+# EXAMPLE
+# INPUT                                             OUTPUT
+# a = "11", b = "1"                                 "100"
+
+
+# Input: 
+# Output: "10101"
+# INPUT                                             OUTPUT
+# a = "1010", b = "1011"                            "10101"
