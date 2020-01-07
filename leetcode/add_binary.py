acc56137293b7ace911c84ff015537d3db66ea14
Prashant Singh <prashant_singh@live.in>
2020-01-06 23:20:40 -0600

    leetcode: soln for `Add Bonary`

--- leetcode/add_binary.py
+++ leetcode/add_binary.py
@@ -11,3 +11,22 @@
 # Output: "10101"
 # INPUT                                             OUTPUT
 # a = "1010", b = "1011"                            "10101"
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = []
+        max_len = max(len(a), len(b))
+        a, b = a.zfill(max_len), b.zfill(max_len)
+        
+        for i in range(max_len-1, -1, -1):
+            if a[i] == '1': carry += 1
+            if b[i] == '1': carry += 1
+            
+            carry, remainder = divmod(carry, 2)
+            result.append(str(remainder))
+            
+        if carry == 1: result.append('1')
+        
+        print("Final:", result)
+        return ''.join(result[::-1])
