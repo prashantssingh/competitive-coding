4023b5444191a3e2541b41cbab61ed06c73588c1
Prashant Singh <prashant_singh@live.in>
2020-01-16 20:14:18 -0600

    leetcode: soln for `Palindrome Number`

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -20,3 +20,16 @@
 
 # FOLLOW UP:
 # Coud you solve it without converting the integer to a string?
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        i, j = 0, len(x)-1
+        while i < j:
+            # print(f"i: {i}--{x[i]}  ||||   j: {j}--{x[j]}")
+            if x[i] != x[j]:
+                return False
+            i += 1
+            j -= 1
+            
+        return True
