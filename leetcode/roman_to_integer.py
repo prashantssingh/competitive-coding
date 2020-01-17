eae27bd90f6d67126e79c03804bc405a99eb3735
Prashant Singh <prashant_singh@live.in>
2020-01-17 00:10:25 -0600

    leetcode: unfinished code for `Roman to Integer`

--- leetcode/roman_to_integer.py
+++ leetcode/roman_to_integer.py
@@ -44,4 +44,17 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        symbol_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        res = 0
+        for i in range(s):
+            pass
+            
