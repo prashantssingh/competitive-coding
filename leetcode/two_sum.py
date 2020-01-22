b7ad110c90e68128b4b43e29d2027feb33437c5c
Prashant Singh <prashant_singh@live.in>
2020-01-21 23:21:24 -0600

    leetcode: prob-desc for `two sum`

---
+++ leetcode/two_sum.py
@@ -0,0 +1,12 @@
+# PROBLEM DESCRIPTION
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# EXAMPLE:
+# INPUT
+# Nums: [2, 7, 11, 15], target: 9,
+
+# EXPLANATION:
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
