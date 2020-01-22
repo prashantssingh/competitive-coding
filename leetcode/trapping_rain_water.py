3f0c69dbd9c3a3eb953540a813903f612ff191a4
Prashant Singh <prashant_singh@live.in>
2020-01-21 23:26:54 -0600

    leetcode: soln for `Trapping Rain Water`

--- leetcode/trapping_rain_water.py
+++ leetcode/trapping_rain_water.py
@@ -10,3 +10,36 @@
 
 # INPUT:                                                   OUTPUT:
 # [0,1,0,2,1,0,1,3,2,1,2,1]                                6
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        waterTrapped = 0
+        if not height:
+            return waterTrapped
+        
+        leftWallMax, rightWallMax =  height[0], height[-1]
+        leftWallArray = [0]*len(height)
+        rightWallArray = [0]*len(height)
+        
+        for i in range(1, len(height)):
+            if height[i] > leftWallMax: 
+                leftWallArray[i] = height[i]
+                leftWallMax = height[i]
+            else: 
+                leftWallArray[i] = leftWallMax
+                    
+        for i in range(len(height)-1, -1, -1):
+            if height[i] > rightWallMax: 
+                rightWallArray[i] = height[i]
+                rightWallMax = height[i]
+            else: 
+                rightWallArray[i] = rightWallMax
+                    
+        # print("LeftWalls:", leftWallArray, ", RightWalls:", rightWallArray)
+        
+        for i in range(1, len(height)-1):
+            minWall = min(leftWallArray[i], rightWallArray[i])
+            waterTrapped += minWall - height[i]
+            
+        return waterTrapped
+        
