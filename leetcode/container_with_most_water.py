3901633733a36566025474d22b69d1699bfbeb46
Prashant Singh <prashant_singh@live.in>
2020-01-21 23:24:06 -0600

    leetcode: soln for `Container With Most Water`

--- leetcode/container_with_most_water.py
+++ leetcode/container_with_most_water.py
@@ -1,3 +1,14 @@
 # PROBLEM DESCRIPTION
 # REFER THIS LINK: https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2963/
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        area = 0
+        i, j = 0, len(height)-1
+        while i < j:
+            contHeight, contWidth = min(height[i], height[j]), j-i
+            area = max(area, contHeight*contWidth)
+            if height[i]<height[j]: i+=1
+            else: j-=1
+            
+        return area
