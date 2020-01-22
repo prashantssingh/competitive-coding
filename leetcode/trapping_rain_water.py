cff277c4a8413037270a764fd143ec745f3ec444
Prashant Singh <prashant_singh@live.in>
2020-01-21 23:26:18 -0600

    leetcode: prob-desc for `Trapping Rain Water`

---
+++ leetcode/trapping_rain_water.py
@@ -0,0 +1,12 @@
+# PROBLEM DESCRIPTION:
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+# compute how much water it is able to trap after raining.
+
+# LINK: https://leetcode.com/problems/trapping-rain-water/
+# ELEVALTION: [0,1,0,2,1,0,1,3,2,1,2,1]. 
+# In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+
+# EXAMPLE:
+
+# INPUT:                                                   OUTPUT:
+# [0,1,0,2,1,0,1,3,2,1,2,1]                                6
