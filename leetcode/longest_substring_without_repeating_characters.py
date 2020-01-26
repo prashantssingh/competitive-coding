6f7e33c83873477d4ec22e5e0f832c98a7e04a1f
Prashant Singh <prashant_singh@live.in>
2020-01-25 23:41:13 -0600

    leetcode: prob-desc for `Longest Substring Without Repeating Characters`

---
+++ leetcode/longest_substring_without_repeating_characters.py
@@ -0,0 +1,17 @@
+# PROBLEM DESCRIPTION:
+# Given a string, find the length of the longest substring without repeating characters.
+#
+# EXAMPLE:
+# Input:                                                  Output:
+# "abcabcbb"                                              3
+# EXPLANATION - The answer is "abc", with the length of 3. 
+#
+# Input:                                                  Output:
+# "bbbbb"                                                 1
+# EXPLANATION - The answer is "b", with the length of 1.
+# 
+# 
+# Input:                                                  Output:
+# "pwwkew"                                                3 
+# EXPLANATION - The answer is "wke", with the length of 3. 
+# Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
