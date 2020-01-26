8e5cc030bc7cf7d0c686eba102e37e4632d9f205
Prashant Singh <prashant_singh@live.in>
2020-01-25 23:35:23 -0600

    leetcode: soln for `Roman to Integer`

--- leetcode/roman_to_integer.py
+++ leetcode/roman_to_integer.py
@@ -54,7 +54,37 @@ class Solution:
             "M": 1000
         }
         
-        res = 0
-        for i in range(s):
-            pass
+        res = i = 0
+        while i < len(s):
+            print(i, res, s[i])
+            if i < len(s)-1:
+                if s[i] == 'I' and s[i+1] == 'V':
+                    res += 4
+                    i += 2
+                elif s[i] == 'I' and s[i+1] == 'X':
+                    res += 9
+                    i += 2
+                elif s[i] == 'X' and s[i+1] == 'L':
+                    res += 40
+                    i += 2
+                elif s[i] == 'X' and s[i+1] == 'C':
+                    res += 90
+                    i += 2
+                elif s[i] == 'C' and s[i+1] == 'D':
+                    res += 400
+                    i += 2
+                elif s[i] == 'C' and s[i+1] == 'M':
+                    res += 900
+                    i += 2
+                else: 
+                    res += symbol_dict[s[i]]
+                    i += 1
+            else: 
+                res += symbol_dict[s[i]]
+                i += 1
+
+        return res
+                
+                
+            
             
