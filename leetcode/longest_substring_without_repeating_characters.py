20e7709b5ecd77c72e8d56e1b5d4f32bc8fe2ef9
Prashant Singh <prashant_singh@live.in>
2020-01-26 23:01:44 -0600

    leetcode: soln for `Longest Substring Without Repeating Characters`

--- leetcode/longest_substring_without_repeating_characters.py
+++ leetcode/longest_substring_without_repeating_characters.py
@@ -15,3 +15,22 @@
 # "pwwkew"                                                3 
 # EXPLANATION - The answer is "wke", with the length of 3. 
 # Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen_set = set()
+        res = curr_str = ''
+        for c in s:
+            if c not in seen_set:
+                print(c)
+                seen_set.add(c)
+                curr_str += c
+                if len(curr_str) > len(res):
+                    res = curr_str
+            else:
+                print(seen_set)
+                seen_set.clear()
+                curr_str = c
+                seen_set.add(c)
+                
+        return len(res)
