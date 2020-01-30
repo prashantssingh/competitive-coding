800430495013fd383e982be314c2200d2a7a4809
Prashant Singh <prashant_singh@live.in>
2020-01-29 23:37:55 -0600

    leetcode: soln for `Copy List with Random Pointer`

--- leetcode/copy_list_with_random_pointer.py
+++ leetcode/copy_list_with_random_pointer.py
@@ -10,3 +10,35 @@
 # random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
 #
 # Refer Link: https://leetcode.com/problems/copy-list-with-random-pointer/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head:
+            return None
+        
+        nodesSeen, ptr1 = dict(), head
+        copyHead = Node(ptr1.val, None, None)
+        ptr2 = copyHead
+        nodesSeen[ptr1] = ptr2
+        ptr1 = ptr1.next
+        while ptr1:
+            ptr2.next = Node(ptr1.val, None, None)
+            ptr2 = ptr2.next
+            nodesSeen[ptr1] = ptr2
+            ptr1 = ptr1.next
+        
+        ptr1, ptr2 = head, copyHead
+        while ptr1:
+            if ptr1.random:
+                ptr2.random = nodesSeen[ptr1.random]
+            ptr1, ptr2 = ptr1.next, ptr2.next
+        
+        return copyHead
