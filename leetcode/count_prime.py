ffd3b05be2703ea20b2e92396d94805766ec548b
Prashant Singh <prashant_singh@live.in>
2020-01-29 23:40:31 -0600

    leetcode: soln for `Count Prime`

--- leetcode/count_prime.py
+++ leetcode/count_prime.py
@@ -8,3 +8,26 @@
 # 
 # EXPLANATION: 
 # There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+class Solution:    
+    def countPrimes(self, n: int) -> int:
+        if n < 3: return 0
+        
+        def prime(num):
+            # print("prime for:", num)
+            res = True
+            for i in range(2, num):
+                # print("i:", i)
+                if num % i == 0:
+                    return False
+
+            return res
+        
+        primes_list = list()
+        num = 2
+        while num < n:
+            if prime(num): 
+                primes_list.append(num)
+            num += 1
+            
+        return len(primes_list)
