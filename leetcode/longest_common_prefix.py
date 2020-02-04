10d97705857303645db0fd2c5dd4e6090753b23e
Prashant Singh <prashant_singh@live.in>
2020-02-04 02:08:24 -0600

    leetcode: prob-desc for 'Longest Common Prefix

---
+++ leetcode/longest_common_prefix.py
@@ -0,0 +1,14 @@
+# PROBLEM DESCRIPTION:
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+# EXAMPLE:
+# INPUT:                                                 OUTPUT:
+# ["flower","flow","flight"]                             "fl"
+# 
+# INPUT:                                                 OUTPUT   
+# ["dog","racecar","car"]                                ""
+# EXPLANATION: There is no common prefix among the input strings.
+
+# NOTE:
+# All given inputs are in lowercase letters a-z.
