a72a72f8008031f7dfc66a73a22488c1ec8d5e7c
Prashant Singh <prashant_singh@live.in>
2020-02-05 23:14:49 -0600

    leetcode: prob-desc for `fibonacci number`

---
+++ leetcode/fibonacci_number.py
@@ -0,0 +1,22 @@
+# PROBLEM DESCRIPTION:
+# The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+# such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+# F(0) = 0,   F(1) = 1
+# F(N) = F(N - 1) + F(N - 2), for N > 1.
+# Given N, calculate F(N).
+
+# EXAMPLES:
+# INPUT:                                               OUTPUT:
+# 2                                                    1
+# EXPLANATION: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+# INPUT:                                               OUTPUT:
+# 3                                                    2
+# EXPLANATION: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+# INPUT:                                               OUTPUT:
+# 4                                                    3
+# EXPLANATION: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+# NOTE: 0 ≤ N ≤ 30.
