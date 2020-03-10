ce4902eacdba68d617372df5f10bb7521796fd8f
Prashant Singh <prashant_singh@live.in>
2020-03-09 20:01:06 -0500

    leetcode: prob-desc for `Contains Duplicate`

---
+++ leetcode/contains_duplicate.py
@@ -0,0 +1,15 @@
+# PROBLEM DESCRIPTION:
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice in the array, and it should 
+# return false if every element is distinct.
+
+# EXAMPLES:
+# INPUT:                                                       OUTPUT:
+# [1,2,3,1]                                                    true
+# 
+# INPUT:                                                       OUTPUT:
+# [1,2,3,4]                                                    false
+# 
+# INPUT:                                                       OUTPUT:
+# [1,1,1,3,3,4,3,2,4,2]                                        true
+
