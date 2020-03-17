54a032fe749330210c3ae3fbb9b2e037c93962da
Prashant Singh <prashant_singh@live.in>
2020-03-16 21:38:49 -0500

    leetcode: brute-force soln for `Shortest Word Distance`

--- leetcode/shortest_word_distance.py
+++ leetcode/shortest_word_distance.py
@@ -14,3 +14,24 @@
 # NOTE:
 # You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
 
+class Solution:
+    def shortestDistance(self, words: List[str], word1: str, word2: str) -> int:
+        words_indices = {
+            word1: [],
+            word2: []
+        }
+        
+        for i in range(len(words)):
+            if words[i] == word1:
+                words_indices[word1].append(i)
+            
+            if words[i] == word2:
+                words_indices[word2].append(i)
+                
+        minimum = len(words)
+        for i in words_indices[word1]:
+            for j in words_indices[word2]:
+                print(i, j)
+                minimum = min(minimum, abs(i-j))
+                
+        return minimum
