412aa6a3bac2626b9098f2f1ffc7de609eb8e205
Prashant Singh <prashant_singh@live.in>
2020-03-16 21:38:20 -0500

    leetcode: prob-desc for `Shortest Word Distance`

---
+++ leetcode/shortest_word_distance.py
@@ -0,0 +1,16 @@
+# PROBLEM DESCRIPTION:
+# Given a list of words and two words word1 and word2, return the shortest distance between these
+#  two words in the list.
+# 
+# EXAMPLE:
+# Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+# 
+# INPUT:                                                            OUTPUT:
+# word1 = “coding”, word2 = “practice”                              3
+# 
+# INPUT:                                                            OUTPUT:
+# word1 = "makes", word2 = "coding"                                 1
+# 
+# NOTE:
+# You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+
