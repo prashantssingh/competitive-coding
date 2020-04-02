4ead571cc532a44f658168990c28a66c42b74570
Prashant Singh <prashant_singh@live.in>
2020-04-01 23:45:00 -0500

    leetcode: prob-desc for `Top K Frequent Elements`

---
+++ leetcode/top_k_frequent_elements.py
@@ -0,0 +1,13 @@
+# PROBLEM DESCRIPTION:
+# Given a non-empty array of integers, return the k most frequent elements.
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# nums = [1,1,1,2,2,3], k = 2               [1,2]
+# 
+# INPUT:                                    OUTPUT:
+# nums = [1], k = 1                         [1]
+# 
+# NOTE:
+# You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+# Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
