dc52349655720a26b5430bed9470332683bff200
Prashant Singh <prashant_singh@live.in>
2020-04-01 23:51:06 -0500

    leetcode: soln for `Single Number`

--- leetcode/single_number.py
+++ leetcode/single_number.py
@@ -11,4 +11,12 @@
 # INPUT:                                    OUTPUT:
 # [4,1,2,1,2]                               4
 
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i<len(nums):
+            if (i+1 == len(nums)) or (nums[i] != nums[i+1]):
+                return nums[i]
+            else:
+                i += 2
