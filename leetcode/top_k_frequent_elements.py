a3a4f77ee969e086076b74f1a05775c24d23015c
Prashant Singh <prashant_singh@live.in>
2020-04-01 23:46:21 -0500

    leetcode: soln using map for `Top K Frequent Elements`

--- leetcode/top_k_frequent_elements.py
+++ leetcode/top_k_frequent_elements.py
@@ -11,3 +11,19 @@
 # NOTE:
 # You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 # Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if not nums: return []
+        
+        freq_dict = dict()
+        for num in nums:
+            freq_dict[num] = freq_dict.get(num, 0) + 1
+        
+        num_freq_list = list()
+        for key in freq_dict:
+            num_freq_list.append((key, freq_dict[key]))
+            
+        num_freq_list = sorted(num_freq_list, key = lambda x: x[1], reverse=True)
+        
+        return [num_freq_list[i][0] for i in range(len(num_freq_list[:k]))]
