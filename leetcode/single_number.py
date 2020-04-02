1437c432b9ec782d0e547a08dc2304addc71b84b
Prashant Singh <prashant_singh@live.in>
2020-04-01 23:50:45 -0500

    leetcode: prob-desc for `Single Number`

---
+++ leetcode/single_number.py
@@ -0,0 +1,14 @@
+# PROBLEM DESCRIPTION:
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+# 
+# NOTE:
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# [2,2,1]                                   1
+# 
+# INPUT:                                    OUTPUT:
+# [4,1,2,1,2]                               4
+
+
