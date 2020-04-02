37adf32915f32f132c2a86f247d6a9331424b0f6
Prashant Singh <prashant_singh@live.in>
2020-04-01 23:47:40 -0500

    leetcode: soln for `Top K Frequent Elements`

--- leetcode/top_k_frequent_elements.py
+++ leetcode/top_k_frequent_elements.py
@@ -11,19 +11,44 @@
 # NOTE:
 # You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 # Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+#
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         if not nums: return []
+#        
+#         freq_dict = dict()
+#         for num in nums:
+#             freq_dict[num] = freq_dict.get(num, 0) + 1
+#        
+#         num_freq_list = list()
+#         for key in freq_dict:
+#             num_freq_list.append((key, freq_dict[key]))
+#            
+#         num_freq_list = sorted(num_freq_list, key = lambda x: x[1], reverse=True)
+#        
+#         return [num_freq_list[i][0] for i in range(len(num_freq_list[:k]))]
+
+import heapq
+from collections import defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if not nums: return []
-        
-        freq_dict = dict()
+        freq_dict = defaultdict(int)
         for num in nums:
-            freq_dict[num] = freq_dict.get(num, 0) + 1
-        
-        num_freq_list = list()
-        for key in freq_dict:
-            num_freq_list.append((key, freq_dict[key]))
+            freq_dict[num] = freq_dict[num] + 1 
             
-        num_freq_list = sorted(num_freq_list, key = lambda x: x[1], reverse=True)
+        if len(freq_dict) <= k:
+            return freq_dict.keys()
         
-        return [num_freq_list[i][0] for i in range(len(num_freq_list[:k]))]
+        heap = []
+        for key in freq_dict:
+            heapq.heappush(heap, (freq_dict[key], key))
+            
+            if len(heap) > k:
+                heapq.heappop(heap)
+           
+        res = list()
+        while heap:
+            res.append(heapq.heappop(heap)[1])
+            
+        return res[::-1]
