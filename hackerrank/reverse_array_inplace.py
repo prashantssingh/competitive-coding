35068a82521e2e8bceedc4d25a28fc49c9cfc645
Prashant Singh <prashant_singh@live.in>
2020-06-08 22:47:47 -0500

    hackerrank: soln for Array-DS reverse string

--- hackerrank/reverse_array_inplace.py
+++ hackerrank/reverse_array_inplace.py
@@ -11,3 +11,34 @@
 # SAMPLE INPUT                                      SAMPLE OUTPUT
 # 0 3 4 2                                           2 4 3 0
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    if not a:
+        return []
+    
+    for i in range(0, len(a)//2):
+        a[i], a[len(a)-i-1] = a[len(a)-i-1], a[i]
+    
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
