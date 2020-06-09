a0c3b11693aa1751b264e34dc9236da34f753421
Prashant Singh <prashant_singh@live.in>
2020-06-08 22:46:56 -0500

    hackerrank: prob-desc for Array-DS reverse string

---
+++ hackerrank/reverse_array_inplace.py
@@ -0,0 +1,13 @@
+# PROBLEM DESCRIPTION:
+# Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
+
+# INPUT FORMAT
+# The first line contains an integer, N (the number of integers in A).
+# The second line contains N  space-separated integers describing A.
+
+# OUTPUT FORMAT
+# Print all N integers in A in reverse order as a single line of space-separated integers.
+
+# SAMPLE INPUT                                      SAMPLE OUTPUT
+# 0 3 4 2                                           2 4 3 0
+
