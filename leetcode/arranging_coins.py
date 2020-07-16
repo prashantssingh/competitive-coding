0d66f18d31ef415a92ffb93ba338bf9d7a816187
Prashant Singh <prashant_singh@live.in>
2020-07-15 23:35:17 -0500

    leetcode: prob-desc for "Arranging coins"

---
+++ leetcode/arranging_coins.py
@@ -0,0 +1,22 @@
+# PROBLEM DESCRIPTION:
+# You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+# Given n, find the total number of full staircase rows that can be formed.
+# n is a non-negative integer and fits within the range of a 32-bit signed integer.
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# n = 5                                     2
+# The coins can form the following rows:
+# ¤
+# ¤ ¤
+# ¤ ¤
+# EXPLANATION: Because the 3rd row is incomplete, we return 2.
+# 
+# INPUT:                                    OUTPUT:
+# n = 8                                     3
+# The coins can form the following rows:
+# ¤
+# ¤ ¤
+# ¤ ¤ ¤
+# ¤ ¤
+# EXPLANATION: Because the 4th row is incomplete, we return 3.
