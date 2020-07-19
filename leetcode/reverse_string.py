2cc9a3ec27a62af8ee7e7bbb10c19b1d9252d7ba
Prashant Singh <prashant_singh@live.in>
2020-07-18 21:16:15 -0500

    leetcode: soln for 'Reverse String'

--- leetcode/reverse_string.py
+++ leetcode/reverse_string.py
@@ -11,3 +11,10 @@
 # INPUT:                                    OUTPUT:
 # ["H","a","n","n","a","h"]                 ["h","a","n","n","a","H"]
 
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i], s[len(s)-i-1] =  s[len(s)-i-1], s[i]
