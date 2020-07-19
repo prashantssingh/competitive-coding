f26a986b7c2c806541a36791031725c2d9af7476
Prashant Singh <prashant_singh@live.in>
2020-07-18 21:13:47 -0500

    leetcode: prob-desc for 'Reverse String'

---
+++ leetcode/reverse_string.py
@@ -0,0 +1,13 @@
+# PROBLEM DESCRIPTION
+# Write a function that reverses a string. The input string is given as an array of characters char[].
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+# You may assume all the characters consist of printable ascii characters.
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# ["h","e","l","l","o"]                     ["o","l","l","e","h"]
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# ["H","a","n","n","a","h"]                 ["h","a","n","n","a","H"]
+
