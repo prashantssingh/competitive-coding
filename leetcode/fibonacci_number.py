9aa5053e13ff83147ffebae8d11c336156013c9e
Prashant Singh <prashant_singh@live.in>
2020-07-19 23:14:11 -0500

    leecode: comment fix

--- leetcode/fibonacci_number.py
+++ leetcode/fibonacci_number.py
@@ -4,21 +4,21 @@
 # F(0) = 0,   F(1) = 1
 # F(N) = F(N - 1) + F(N - 2), for N > 1.
 # Given N, calculate F(N).
-
+# 
 # EXAMPLES:
 # INPUT:                                               OUTPUT:
 # 2                                                    1
 # EXPLANATION: F(2) = F(1) + F(0) = 1 + 0 = 1.
-
+# 
 # INPUT:                                               OUTPUT:
 # 3                                                    2
 # EXPLANATION: F(3) = F(2) + F(1) = 1 + 1 = 2.
-
+# 
 # INPUT:                                               OUTPUT:
 # 4                                                    3
 # EXPLANATION: F(4) = F(3) + F(2) = 2 + 1 = 3.
- 
-
+#  
+# 
 # NOTE: 0 ≤ N ≤ 30.
 
 class Solution:
--- leetcode/first_unique_character.py
+++ leetcode/first_unique_character.py
@@ -1,13 +1,12 @@
 # PROBLEM DESCRIPTION
 # Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
-
-# Examples:
-
-# s = "leetcode"
-# return 0.
-
-# s = "loveleetcode"
-# return 2.
- 
-
+# 
+# EXAMPLES:
+# INPUT:                                               OUTPUT:
+# s = "leetcode"                                       0
+# 
+# EXAMPLES:
+# INPUT:                                               OUTPUT:
+# s = "loveleetcode"                                   2
+#  
 # Note: You may assume the string contains only lowercase English letters.
