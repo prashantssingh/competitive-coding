c147f919e944a0b50becf049ca96520d7f1da3e8
Prashant Singh <prashant_singh@live.in>
2020-07-20 21:41:36 -0500

    leetcode: prob-desc for `Valid Anagram`

---
+++ leetcode/valid_anagram.py
@@ -0,0 +1,17 @@
+#  PROBLEM DESCRIPTION:
+# Given two strings s and t , write a function to determine if t is an anagram of s.
+# 
+# EXAMPLES:
+# INPUT                                               OUTPUT:
+# s = "anagram", t = "nagaram"                        true
+# 
+# EXAMPLES:
+# INPUT                                               OUTPUT:
+# s = "rat", t = "car"                                false
+# 
+# NOTE:
+# You may assume the string contains only lowercase alphabets.
+# 
+# FOLLOW UP:
+# What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
