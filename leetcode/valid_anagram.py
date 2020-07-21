d6dd96d81683b17473a7045dc446eb996a82e675
Prashant Singh <prashant_singh@live.in>
2020-07-20 21:42:24 -0500

    leetcode: soln for `Valid Anagram`

--- leetcode/valid_anagram.py
+++ leetcode/valid_anagram.py
@@ -15,3 +15,21 @@
 # FOLLOW UP:
 # What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_dict = dict()
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+            
+        t_dict = dict()
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+            
+        for key in s_dict:
+            if key not in t_dict or s_dict[key] != t_dict[key]:
+                return False
+            
+        return True
