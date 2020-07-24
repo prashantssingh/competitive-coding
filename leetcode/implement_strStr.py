60c339982de491036adab75b8047fe37143ef958
Prashant Singh <prashant_singh@live.in>
2020-07-23 23:40:38 -0500

    leetcode: prob-desc for `Implement strStr()`

---
+++ leetcode/implement_strStr.py
@@ -0,0 +1,17 @@
+# PROBLEM DESCRIPTION:
+# Implement strStr().
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# 
+# EXAMPLE:
+# INPUT                                                     OUTPUT:
+# haystack = "hello", needle = "ll"                         2
+# 
+# EXAMPLE:
+# INPUT                                                     OUTPUT:
+# haystack = "aaaaa", needle = "bba"                        -1
+# 
+# CLARIFICATIONS:
+# What should we return when needle is an empty string? This is a great question to ask during an interview.
+# 
+# For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's 
+# strstr() and Java's indexOf().
