b4e39f674a8654e12d6a390882e5953e5564d8bc
Prashant Singh <prashant_singh@live.in>
2020-07-23 23:41:18 -0500

    leetcode: soln for `Implement strStr()`

--- leetcode/implement_strStr.py
+++ leetcode/implement_strStr.py
@@ -15,3 +15,12 @@
 # 
 # For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's 
 # strstr() and Java's indexOf().
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        L, n = len(needle), len(haystack)
+
+        for start in range(n - L + 1):
+            if haystack[start: start + L] == needle:
+                return start
+        return -1
