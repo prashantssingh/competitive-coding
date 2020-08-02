0fb3a2338d65068ffa2f7e2b3f694cae4c47f5b1
Prashant Singh <prashant_singh@live.in>
2020-08-01 20:53:08 -0500

    leetcode: prob-dec for `Angle Between Hands of a Clock`

---
+++ leetcode/angle_between_hands_of_clock.py
@@ -0,0 +1,29 @@
+# PROBLEM DESCRIPTION:
+# Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour 
+# and the minute hand.
+# 
+# # EXAMPLE:
+# INPUT:                                    OUTPUT:
+# hour = 12, minutes = 30                   165
+# 
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# hour = 3, minutes = 30                    75
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# hour = 3, minutes = 15                    7.5
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# hour = 4, minutes = 50                    155
+# 
+# EXAMPLE:
+# INPUT:                                    OUTPUT:
+# hour = 12, minutes = 0                    0
+# 
+# CONSTRAINTS:
+# 1 <= hour <= 12
+# 0 <= minutes <= 59
+# Answers within 10^-5 of the actual value will be accepted as .
