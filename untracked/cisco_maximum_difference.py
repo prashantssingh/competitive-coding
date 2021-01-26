253f38455608a66c75afc1fda624755746ccbfa0
Prashant Singh <prashant_singh@live.in>
2021-01-25 18:16:42 -0600

    untracked: "Maximum Difference" from Cisco

---
+++ untracked/cisco_maximum_difference.py
@@ -0,0 +1,17 @@
+def max_difference(arr):
+    min_arr = arr.copy()
+    for i in range(1, len(arr)):
+        min_arr[i] = min(arr[i], arr[i-1])
+
+    max_arr = arr.copy()
+    for i in range(len(arr)-2, -1, -1):
+        max_arr[i] = max(max_arr[i], max_arr[i+1])
+
+    diff_arr = [max_arr[i]-min_arr[i] for i in range(len(arr))]
+
+    return max(diff_arr)
+
+print(max_difference([1,2,3,4,5]))
+print(max_difference([7,9,5,6,3,2]))
+print(max_difference([2,3,10,6,4,8,1]))
+print(max_difference([5,4,3,2,1]))
